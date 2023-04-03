fb672c12c75596fd09baace8decf3c519bc181d0
Kevin Alejandro <lealtorreskevin@gmail.com>
2023-04-03 17:10:29 -0600

    se finalizó con el index.html

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,11 @@ function maullido() {
     audio.play();
     alert("¡Descubriste una función secreta, miau!, hay más en todas las páginas...🐱")
 }
+
+const select = document.getElementById('seleccion');
+select.addEventListener('change', function() {
+  const seleccionado = this.value;
+  if (seleccionado !== '') {
+    window.location.href = seleccionado;
+  }
+});
